371a79a45b692e8fc2981d82ae676c2bcad6f0d2
juan jesus ojeda castelo <joc028@ual.es>
2022-02-10 23:06:15 +0100

    Agregar archivos de proyecto.

---
+++ CTS/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        int i = 10;
+
+        // Type Integer
+
+        Type tInteger = i.GetType();
+
+        Console.WriteLine("Integer information: ");
+
+        Console.WriteLine(tInteger.BaseType);
+
+        Console.WriteLine(tInteger.Name);
+
+        Console.WriteLine(tInteger.FullName);
+
+        Console.WriteLine(tInteger.Namespace);
+
+        Console.WriteLine();
+
+        // Type String
+
+        string text = "computer";
+
+        Type tString = text.GetType();
+
+        Console.WriteLine("String information: ");
+
+        Console.WriteLine(tString.BaseType);
+
+        Console.WriteLine(tString.Name);
+
+        Console.WriteLine(tString.FullName);
+
+        Console.WriteLine(tString.Namespace);
+
+
+
+    }
+}
+
