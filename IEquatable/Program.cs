e9331587054cd2fa7841026dc101dca1fd2171a7
juan jesus ojeda castelo <joc028@ual.es>
2022-02-24 23:05:42 +0100

    Adding project IEquatable.

---
+++ IEquatable/Program.cs
@@ -0,0 +1,38 @@
+﻿using IEquatable;
+using System.Collections;
+
+namespace Equals
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Usuario usuario1 = new("pedro", "miclave");
+            Usuario usuario2 = new("pedro", "otraclave");
+
+            // ESCENARIO 1: True (correcto)
+            Console.WriteLine("usuario1.Equals(usuario2): {0}", usuario1.Equals(usuario2));
+
+            // ESCENARIO 2: False (posible error)
+            Object obj = usuario2;
+            Console.WriteLine("usuario1.Equals(obj): {0}", usuario1.Equals(obj));
+
+            // ESCENARIO 3: False (posible error)          
+            ArrayList listaA = new();
+            listaA.Add(usuario1);
+            Console.WriteLine("lista.Contains(usuario2): {0}", listaA.Contains(usuario2));
+
+            // ESCENARIO 4: True (correcto)
+            List<Usuario> listaB = new List<Usuario>();
+            listaB.Add(usuario1);
+            Console.WriteLine("listaB<Usuario>.Contains(usuario2): {0}", listaB.Contains(usuario2));
+
+
+
+
+        }
+
+    }
+}
---
+++ IEquatable/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEquatable
+{
+    public class Usuario : IEquatable<Usuario>
+    {
+        public string Nombre;
+        public string PalabraDePaso;
+        public Usuario(string nombre, string palabraDePaso)
+        {
+            this.Nombre = nombre;
+            this.PalabraDePaso = palabraDePaso;
+        }
+
+        public bool Equals(Usuario? otroUsuario)
+        {
+            if (otroUsuario is null) return false;
+            return (this.Nombre.Equals(otroUsuario.Nombre));
+
+        }
+
+        // Se debe reemplazar Object.Equals y Object.GetHashCode y sobrecargar == !=
+
+    }
+}
