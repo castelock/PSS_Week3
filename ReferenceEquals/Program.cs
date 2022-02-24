3ee129d674f4e3bbac919baebf63c25e52761603
juan jesus ojeda castelo <joc028@ual.es>
2022-02-24 18:53:55 +0100

    Adding the project ReferenceEquals.

---
+++ ReferenceEquals/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReferenceEquals
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            object o = null;
+            object p = null;
+            object q = new Object();
+
+            Console.WriteLine(Object.ReferenceEquals(o, p));
+            p = q;
+            Console.WriteLine(Object.ReferenceEquals(p, q));
+            Console.WriteLine(Object.ReferenceEquals(o, p));
+
+
+
+        }
+
+    }
+}
