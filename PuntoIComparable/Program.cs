7009596b093c85ba1fb4a5494d9f9479c18d28da
juan jesus ojeda castelo <joc028@ual.es>
2022-03-02 18:41:11 +0100

    Creation of the project PuntoIComparable.

---
+++ PuntoIComparable/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace PuntoIComparable
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Punto p1 = new(2, 3);
+            Punto p2 = new(2, 3);
+            Punto p3 = new(3, 2);
+            Punto p4 = new(1, 1);
+
+            Console.WriteLine("p1 = " + p1);
+            Console.WriteLine("p2 = " + p2);
+            Console.WriteLine("p3 = " + p3);
+            Console.WriteLine("p4 = " + p4);
+
+            Console.WriteLine(p1.Equals(null));     // False
+            Console.WriteLine(p1.Equals(p2));       // True
+            Console.WriteLine(p1 == p2);            // True
+            Console.WriteLine(p1.CompareTo(p2));    // 0
+            Console.WriteLine(p1.CompareTo(p3));    // 0
+            Console.WriteLine(p1.Equals(p3));       // False
+            Console.WriteLine(p1 == p3);            // False
+            Console.WriteLine(p1.CompareTo(p4));    // 1
+            Console.WriteLine(p4.CompareTo(p1));    // -1
+            Console.WriteLine(p1.CompareTo(null));  // 1
+
+            List<Punto> Puntos = new List<Punto>();
+            Puntos.Add(p1);
+            Puntos.Add(p2);
+            Puntos.Add(p3);
+            Puntos.Add(p4);
+
+            Console.WriteLine("\nLista\n=====");
+            foreach (Punto p in Puntos) Console.WriteLine(p);
+
+            Puntos.Sort();
+            Console.WriteLine("\nLista\n=====");
+            foreach (Punto p in Puntos) Console.WriteLine(p);
+
+
+        }
+
+    }
+}
---
+++ PuntoIComparable/Punto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoIComparable
+{
+    public class Punto:IComparable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Punto(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public override int GetHashCode()
+        {
+            return X ^ Y;
+        }
+
+        public override bool Equals(Object? obj)
+        {
+            if (obj is not Punto p) return false; // otro tipo o null
+            return (X == p.X) && (Y == p.Y);
+        }
+
+        public static bool operator ==(Punto obj1, Punto obj2)
+        {
+            return (obj1.X == obj2.X) && (obj1.Y == obj2.Y);
+        }
+        public static bool operator !=(Punto obj1, Punto obj2)
+        {
+            return !(obj1 == obj2);
+        }
+
+        public int CompareTo(Object? obj)
+        {
+            if (obj == null) return 1;
+            if (obj is Punto punto2)
+            {
+                double dis1 = Math.Sqrt(Math.Pow(this.X, 2.0) + Math.Pow(this.Y, 2.0));
+                double dis2 = Math.Sqrt(Math.Pow(punto2.X, 2.0) + Math.Pow(punto2.Y, 2.0));
+                return dis1.CompareTo(dis2);  // se compara la distancia al origen
+            }
+            throw new ArgumentException("El objeto no es Punto");
+        }
+
+        public override string ToString()
+        {
+            return "(" + this.X + "," + this.Y + ")";
+        }
+
+    }
+}
