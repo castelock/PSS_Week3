5ee8fb588cd19d03fd232620b3f5efd4f1bb9496
juan jesus ojeda castelo <joc028@ual.es>
2022-03-02 17:46:24 +0100

    Creation of the project IStructuralEquatable.

---
+++ IStructuralEquatable/NanComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IStructuralEquatable
+{
+    internal class NanComparer : IEqualityComparer
+    {
+        public new bool Equals(object? x, object? y)
+        {
+            if (x is float)
+                return (float)x == (float)y;
+            else if (x is double)
+                return (double)x == (double)y;
+            else
+                return EqualityComparer<object>.Default.Equals(x, y);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return EqualityComparer<object>.Default.GetHashCode(obj);
+        }
+    }
+}
---
+++ IStructuralEquatable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace IStructuralEquatable
+{
+    public class Program
+    {
+
+       
+        public static void Main(string[] args)
+        {
+            var t1 = Tuple.Create(12.3, Double.NaN, 16.4);
+            var t2 = Tuple.Create(12.3, Double.NaN, 16.4);
+
+            // Call default Equals method.
+            Console.WriteLine(t1.Equals(t2));
+
+            System.Collections.IStructuralEquatable equ = t1;
+            // Call IStructuralEquatable.Equals using default comparer.
+            Console.WriteLine(equ.Equals(t2, EqualityComparer<object>.Default));
+
+            // Call IStructuralEquatable.Equals using
+            // StructuralComparisons.StructuralEqualityComparer.
+            Console.WriteLine(equ.Equals(t2,
+                              StructuralComparisons.StructuralEqualityComparer));
+
+            // Call IStructuralEquatable.Equals using custom comparer.
+            Console.WriteLine(equ.Equals(t2, new NanComparer()));
+
+        }
+
+    }
+}
